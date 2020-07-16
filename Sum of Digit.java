49c6d42142500793a6fd20ec1fbb48ce88762cc0
Nandhini0926 <67898462+Nandhini0926@users.noreply.github.com>
2020-07-16 21:20:43 +0530

    Add files via upload

---
+++ Sum of Digit.java
@@ -0,0 +1,28 @@
+2. Write a Java program and compute the sum of the digits of an integer 
+ ex - Number - 25 , Output =7
+
+
+
+import java.util.Scanner;
+public class Sum {
+
+  public static void main(String[] args)
+    {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Input an integer: ");
+      int digits = in.nextInt();
+	  System.out.println("The sum is " + sumDigits(digits));
+    }
+
+ public static int sumDigits(long n) {
+		int result = 0;
+		
+		while(n > 0) {
+			result += n % 10;
+			n /= 10;
+		}
+		
+		return result;
+	}
+	
+ }
